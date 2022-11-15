9a0310e618aefdd9716274265f202822eb54462b
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-11-15 15:47:25 +0500

    conditional renderring in joke component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './style.css'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
+// import Hero from './components/Hero'
+// import Navbar from './components/Navbar'
 import Card from './components/Card'
 // import Contact from './components/Contact'
 // import logo from './image/mr-whiskerson.png'
-// import Joke from './components/Joke'
-// import jokesData from './components/jokeData'
+import Joke from './components/Joke'
+import jokesData from './components/jokeData'
 import data from './components/data'
 
 function App() {
-  // console.log(data)
-  // const jokeElements = jokesData.map((joke, i) => {
-  //   return <Joke setup={joke.setup} punchline={joke.punchline} />
-  // })
-  const cards = data.map((item, i) => {
-    return <Card key={item.id} {...item} />
+  console.log(data)
+  const jokeElements = jokesData.map((joke, i) => {
+    return <Joke setup={joke.setup} punchline={joke.punchline} key={i} />
   })
+  // const cards = data.map((item, i) => {
+  //   return <Card key={item.id} {...item} />
+  // })
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
       {/* <Card /> */}
       {/* <Contact
         img="./image/mr-whiskerson.png"
@@ -31,8 +31,8 @@ function App() {
         downvotes={2}
         comments={{ author: 'Haroon', body: 'He is auther', title: 'None' }}
       /> */}
-      {/* {jokeElements} */}
-      <section className="cards-list">{cards}</section>
+      {jokeElements}
+      {/* <section className="cards-list">{cards}</section> */}
     </div>
   )
 }
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 export default function Joke(props) {
+  const [isShown, setIsShown] = React.useState(true)
+  /**
+   * Challenge:
+   * - Only display the punchline paragraph if `isShown` is true
+   */
+  function toggleShown() {
+    setIsShown((prevShown) => !prevShown)
+  }
   return (
     <div>
-      {props.setup && <h3>Setup: {props.setup}</h3>}
-      <p>Punchline: {props.punchline}</p>
+      {props.setup && <h3>{props.setup}</h3>}
+      {isShown && <p>{props.punchline}</p>}
+      <button onClick={toggleShown}>Show Punchline</button>
       <hr />
     </div>
   )
